cd11d79535c3e86f2823f4ec5d4cb81261c5195d
IIpocTo <allxf95@gmail.com>
2017-03-26 15:51:34 +0300

    Styling for 404 page

--- src/app/pages/not-found/NotFound.jsx
+++ src/app/pages/not-found/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class NotFound extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container-fluid">
         <h3>404 page not found</h3>
         <p>We are sorry but the page you are looking for does not exist.</p>
       </div>
