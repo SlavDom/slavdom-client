33effc0d147113a8e0984d410723cf82f4a7da67
George <eakarpov@yandex.ru>
2017-03-31 19:05:42 +0300

    Updated news view

--- src/app/pages/news/News.jsx
+++ src/app/pages/news/News.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
+import './News.css';
 
 class News extends React.Component {
 
   constructor(props, context) {
     super(props, context);
     this.state = {
-      newsList: null,
+      news: null,
       lang: props.lang,
     };
   }
@@ -16,7 +17,7 @@ class News extends React.Component {
     axios.get(`/api/news/get?lang=${this.state.lang}&theme=${this.props.params.theme}`)
       .then((response) => {
         this.setState({
-          newsList: response.data.data,
+          news: response.data.data,
         });
       })
       .catch((error) => {
@@ -33,12 +34,12 @@ class News extends React.Component {
         .then((response) => {
           if (response !== null) {
             this.setState({
-              newsList: response.data.data,
+              news: response.data.data,
               lang: currentValue,
             });
           } else {
             this.setState({
-              newsList: null,
+              news: null,
               lang: currentValue,
             });
           }
@@ -51,20 +52,64 @@ class News extends React.Component {
 
   render() {
     let newsPage = null;
-    if (this.state.newsList !== null) {
-      newsPage = (<div>
-        <h1>{this.state.newsList.title}</h1>
-        <p>This is a news page with theme: {this.props.params.theme}.</p>
-        <div>
-          {this.state.newsList.fullText}
+    let newsCommentaries = null;
+    if (this.state.news !== null) {
+      if (this.state.news.commentaries.length > 0) {
+        newsCommentaries = (<div>
+          There is some commentaries on this news.
+        </div>);
+      } else {
+        newsCommentaries = (<div>
+          There is no commentaries on this news yet.
+        </div>);
+      }
+      newsPage = (<div id="newsPage" className="col-md-offset-1 col-md-6">
+        <h2>{this.state.news.title}</h2>
+        <hr />
+        <div className="row">
+          <div className="col-md-3">
+            <img src="/images/default.png" width="100%" alt="Something describing news" />
+          </div>
+          <div className="col-md-6">
+            <ul>
+              <li>
+                Category:
+              </li>
+              <li>
+                Date: {this.state.news.createdAt}
+              </li>
+              <li>
+                Author:
+              </li>
+              <li>
+                Views:
+              </li>
+            </ul>
+          </div>
         </div>
+        <br />
+        <div className="row">
+          <p className="newsText">
+            {this.state.news.fullText}
+          </p>
+        </div>
+        <hr />
+        <div className="row">
+          <div className="col-md-offset-1 col-md-5">
+            <h4>
+              Commentaries
+            </h4>
+            {newsCommentaries}
+          </div>
+        </div>
+        <br />
       </div>);
     } else {
-      newsPage = (<div>
+      newsPage = (<div className="row">
           There is no translation for this news yet.
         </div>);
     }
-    return (<div>{newsPage}</div>);
+    return (<div className="row">{newsPage}</div>);
   }
 }
 
